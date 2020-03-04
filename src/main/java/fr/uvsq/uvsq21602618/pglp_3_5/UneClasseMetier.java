feef8a479a17a6385fb369185944451c39fb6312
Nathalie BERNARD <nathalie.bernard@ens.uvsq.fr>
2020-03-04 10:28:19 +0100

    Ajout constructeur et MAJ de uneMethodeMetier dans uneCLasseMetier

--- src/main/java/fr/uvsq/uvsq21602618/pglp_3_5/UneClasseMetier.java
+++ src/main/java/fr/uvsq/uvsq21602618/pglp_3_5/UneClasseMetier.java
@@ -1,9 +1,15 @@
 package fr.uvsq.uvsq21602618.pglp_3_5;
 
-public class UneClasseMetier implements Affichage{
+public class UneClasseMetier{
 
+	private AffichageTemps AT;
+	
+	public UneClasseMetier(AffichageTemps AT) {
+		this.AT=AT;
+	}
 	public void uneMethodeMetier() {
-		
+		this.AT.affichageTemporel(": Début de uneMethodeMetier");
 		//Traitement métier
+		this.AT.affichageTemporel(": Fin de uneMethodeMetier");
 	}
 }
