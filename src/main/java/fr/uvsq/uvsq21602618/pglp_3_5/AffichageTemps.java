47f0bfc84de9dc982c128e22460ffdd9993d7b1e
Nathalie BERNARD <nathalie.bernard@ens.uvsq.fr>
2020-03-04 10:24:16 +0100

    creation methode affichage temporel dans l'interface Affichage puis implementation dans la classe AffichageTemps

--- src/main/java/fr/uvsq/uvsq21602618/pglp_3_5/AffichageTemps.java
+++ src/main/java/fr/uvsq/uvsq21602618/pglp_3_5/AffichageTemps.java
@@ -1,5 +1,12 @@
 package fr.uvsq.uvsq21602618.pglp_3_5;
 
-public class AffichageTemps {
+import java.time.LocalDateTime;
+
+public class AffichageTemps implements Affichage {
+
+	public void affichageTemporel(String descriptif) {
+		System.out.println(LocalDateTime.now() + descriptif);
+		
+	}
 
 }
